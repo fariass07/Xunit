a953cb2615b1a2ce4c921d4e221627769cde86ea
bernardo.farias <bernardo.farias@PLE109D23>
2024-07-09 15:59:02 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Calculadora
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Teste.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp1
+{
+    public class CalculadoraTeste
+    {
+        [Fact]
+        public void TesteSomar()
+        {
+            double resultado = Calculadora.Somar(3, 2);
+            Assert.Equal(5, resultado);
+        }
+
+        [Fact]
+        public void TesteSubtrair()
+        {
+            double resultado = Calculadora.Subtrair(3, 2);
+            Assert.Equal(1, resultado);
+        }
+
+        [Fact]
+        public void TesteMultiplicar()
+        {
+            double resultado = Calculadora.Multiplicar(3, 2);
+            Assert.Equal(6, resultado);
+        }
+
+        [Fact]
+        public void TesteDividir()
+        {
+            double resultado = Calculadora.Dividir(3, 2);
+            Assert.Equal(1.5, resultado);
+        }
+
+        [Fact]
+        public void TesteDividirZero()
+        {
+            double resultado = Calculadora.Dividir(3, 0);
+            Assert.Equal(double.PositiveInfinity, resultado);
+        }
+    }
+}   
